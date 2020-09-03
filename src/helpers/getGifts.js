2483998f69dbb520b60151adbcb949d25af90d03
Cesar Alberto Nava Chavez <ing.cesarchaveznava@gmail.com>
2020-09-03 18:47:09 -0500

    Docs agregado

--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -1,5 +1,5 @@
 export const getGifts = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=3&api_key=`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=3&api_key=RYxKvg5tM8Mh6iNRSlkmjaB3XreyKtW8`;
     const response = await fetch(url);
     const {data} = await response.json();
 
