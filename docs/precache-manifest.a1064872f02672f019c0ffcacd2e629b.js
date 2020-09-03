b2c71946c0ee7e3e7dd43707b0df62b8c7375ead
Cesar Alberto Nava Chavez <ing.cesarchaveznava@gmail.com>
2020-09-03 18:47:26 -0500

    Docs Agregado

---
+++ docs/precache-manifest.a1064872f02672f019c0ffcacd2e629b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86199615e23ffa935f5c73298bab69cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bd3258219dafeca4836",
+    "url": "/static/css/2.aaa27ca9.chunk.css"
+  },
+  {
+    "revision": "aee2b305223932a64de8",
+    "url": "/static/css/main.02c3056a.chunk.css"
+  },
+  {
+    "revision": "0bd3258219dafeca4836",
+    "url": "/static/js/2.c08af57d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c08af57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee2b305223932a64de8",
+    "url": "/static/js/main.75030273.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
