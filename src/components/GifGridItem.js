7240a083974b537955360b257112f75d9d79b3e8
Cesar Alberto Nava Chavez <ing.cesarchaveznava@gmail.com>
2020-09-03 19:32:54 -0500

    Pruebas de GifGridItem

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 export const GifGridItem = ( { title, url} ) => {
     return (
@@ -17,4 +18,9 @@ export const GifGridItem = ( { title, url} ) => {
             </div>
         </div>
     )
+}
+
+GifGridItem.prototype = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
 }
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -4,10 +4,32 @@ import { GifGridItem } from '../../components/GifGridItem';
 
 
 describe('Pruebas del componente GifGridItem', () => {
-    test('Debe mostrar el componente correctamente', () => {
-        const wrapper = shallow(<GifGridItem />)
+    const title = 'crybaby GIF'
+    const url = 'https://media3.giphy.com/media/ygAmzS6qTOkzxG0VsB/giphy.gif?cid=ef5ad807tpsqr3euuhgy4cxigprybtps0f2qg0y73q8x3cqq&rid=giphy.gif';
+    const wrapper = shallow(<GifGridItem title={title} url={url} />)
 
+    test('Debe mostrar el componente correctamente', () => {
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debe de tener un parrafo con el titulo', () => {
+        const p = wrapper.find('p'); 
+        expect(p.text().trim()).toBe(title);
+    });
+    
+
+    test('Debe de tener la imagen igual a la url', () => {
+        const img = wrapper.find('img');
+
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    });
+
+    test('Debe tener la clase de animacion animate__pulse', () => {
+        const div = wrapper.find('.column');
+        const classes = div.prop('className');
+
+        expect(classes).toMatch('animate__pulse');
     })
     
-})
+});
